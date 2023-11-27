d050cce5fc70be92fbcd2dd4ff0d1ec164f6219e
rahul <mrsvaz@icloud.com>
2023-11-27 19:25:25 +0530

    added cart animation

--- lib/implicit_examples/animated_shopping_cart_button.dart
+++ lib/implicit_examples/animated_shopping_cart_button.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCartButton extends StatelessWidget {
+class ShoppingCartButton extends StatefulWidget {
   const ShoppingCartButton({super.key});
+
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -10,16 +17,41 @@ class ShoppingCartButton extends StatelessWidget {
         title: const Text('Shopping Cart'),
       ),
       body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            width: isExpanded ? 200 : 80,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: isExpanded ? Colors.green : Colors.blue,
+              borderRadius: BorderRadius.circular(isExpanded ? 30 : 10.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  isExpanded ? Icons.check : Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+                if (isExpanded)
+                  AnimatedOpacity(
+                    duration: const Duration(milliseconds: 1500),
+                    opacity: isExpanded ? 1 : 0,
+                    child: const Text(
+                      'Added To Cart',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  )
+              ],
+            ),
           ),
         ),
       ),
